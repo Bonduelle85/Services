5a0eb9759fa0de1f51429a07fa4387a731558926
Anton Gorokhov <bonduelle85@gmail.com>
2024-09-11 20:12:39 +0300

    MyService, lifeCycle, coroutines

---
+++ app/src/main/java/com/example/services/MyService.kt
@@ -0,0 +1,51 @@
+package com.example.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.os.Message
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyService : Service() {
+
+    val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        scope.launch {
+            log("onCreate")
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        scope.launch {
+            repeat(50) {
+                delay(1000)
+                log("onStartCommand..$it")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.launch {
+            log("onDestroy")
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? { TODO("Not yet implemented") }
+
+    private fun log(message: String) {
+        Log.d("MyService", "$this: $message")
+    }
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, MyService::class.java)
+    }
+}
