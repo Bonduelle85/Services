8f3be41b3398dd09d7955208964899427ffbfc90
Anton Gorokhov <bonduelle85@gmail.com>
2024-09-13 15:30:29 +0300

    >Oreo, Oreo, Tiramisu

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var notification: Notification
+    private lateinit var notificationChannel: NotificationChannel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +25,49 @@ class MainActivity : AppCompatActivity() {
         binding.service.setOnClickListener {
             startService(MyService.newIntent(this))
         }
+
+        binding.foregroundService.setOnClickListener {
+            requestNotificationPermission()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationChannel.description = CHANNEL_DESCRIPTION
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    // TIRAMISU. For verbose -> https://stepik.org/lesson/709339/step/1?unit=709902
+    private fun requestNotificationPermission() {
+        if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            startActivity(intent)
+        } else {
+            showNotification()
+        }
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "CHANNEL_ID"
+        private const val CHANNEL_NAME = "CHANNEL NAME"
+        private const val CHANNEL_DESCRIPTION = "CHANNEL DESCRIPTION"
     }
 }
